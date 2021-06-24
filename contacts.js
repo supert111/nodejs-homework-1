8582b9fc7035f8b8d4a162701f198a12122e70fb
supert111 <sportsmen@bigmir.net>
2021-06-24 14:12:58 +0300

    start-home-work
    start-home-work

---
+++ contacts.js
@@ -0,0 +1,21 @@
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+    // ...твой код
+  }
+  
+  function getContactById(contactId) {
+    // ...твой код
+  }
+  
+  function removeContact(contactId) {
+    // ...твой код
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твой код
+  }
